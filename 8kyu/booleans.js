420b71062deee458b012990b3548dc0d573dd2d6
Karla G. <k.grajalesluna@gmail.com>
2026-01-22 17:10:26 +0000

    feat: function that return string depends on the boolean received

---
+++ 8kyu/booleans.js
@@ -0,0 +1,20 @@
+// Complete the method that takes a boolean value and return 
+// a "Yes" string for true, or a "No" string for false.
+
+function boolToWord(bool){
+    if(bool === true){
+        return "Yes";
+    }else{
+        return "no";
+    }
+}
+
+// Alternative solution using ternary operator
+
+// function boolToWord(bool) {
+//   return bool ? "Yes" : "No";
+// }
+
+
+// explanation: we used the ternate operator to evaluate the condition 
+// condition ? valor_si_true : valor_si_false
