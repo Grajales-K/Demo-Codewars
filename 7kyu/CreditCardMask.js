f7e23ff04dc9e979ab80029906548d98d8f04b48
Karla G. <k.grajalesluna@gmail.com>
2026-01-27 23:05:09 +0000

    fix: fixed a function to return all mask characters except for the last four

--- 7kyu/CreditCardMask.js
+++ 7kyu/CreditCardMask.js
@@ -1,11 +1,36 @@
 // return masked string
 function maskify(cc) {
-  console.log(cc);
+  // console.log(cc);
 
   if (cc.length <= 4) {
     return cc;
   }
 
+  const lastFour = cc.slice(-4);
+  // console.log(lastFour);
+
+  const maskingPart = cc.slice(0, -4);
+  // console.log(maskingPart);
+
+  const masked = "#".repeat(maskingPart.length);
+  console.log(masked);
+
+  // console.log(masked + lastFour);
+
+  return masked + lastFour;
+}
+
+module.exports = maskify;
+
+
+
+
+function maskify2(cc) {
+  console.log(cc);
+
+  if (cc.length <= 4) {
+    return cc;
+  }
   const lastFour = cc.slice(-4);
   console.log(lastFour);
 
@@ -20,4 +45,4 @@ function maskify(cc) {
   return masked + lastFour;
 }
 
-maskify( "jjsksjfi100045670");
+maskify2( "jjsksjfi100045670"); //invoke the function to run node.
