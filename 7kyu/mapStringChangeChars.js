75e154c468485a7fbb2d6edd546038c5291dd205
Karla G. <k.grajalesluna@gmail.com>
2026-02-04 23:02:32 +0000

    feat: add gordon roar function with vowel replacement and word suffixes

---
+++ 7kyu/mapStringChangeChars.js
@@ -0,0 +1,34 @@
+// Gordon Ramsay shouts. He shouts and swears. There may be 
+// something wrong with him.
+
+// Anyway, you will be given a string of four words. Your 
+// job is to turn them in to Gordon language.
+
+// Rules:
+
+// Obviously the words should be Caps, Every word should 
+// end with '!!!!', Any letter 'a' or 'A' should become '@', 
+// Any other vowel should become '*'.
+
+function gordon(a) {
+  let words = a.toUpperCase().split(" ");
+  let result = [];
+
+  for (let word of words) {
+    let transformWord = "";
+
+    for (let i = 0; i < word.length; i++) {
+      let char = word[i];
+
+      if (char === "A") {
+        transformWord += "@";
+      } else if ("EIOU".includes(char)) {
+        transformWord += "*";
+      } else {
+        transformWord += char;
+      }
+    }
+    result.push(transformWord + "!!!!");
+  }
+  return result.join(" ");
+}
