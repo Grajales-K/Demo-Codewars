99caabd3138ee768e7886e1985ef3db386f0a842
Karla G. <k.grajalesluna@gmail.com>
2026-02-05 23:33:12 +0000

    test: add coordinate test cases with nested array passing one item from the list.

--- 8kyu/slopeMaths.js
+++ 8kyu/slopeMaths.js
@@ -31,3 +31,21 @@ function slope(points) {
   return result.toString();
 }
 
+
+console.log("----------- testing Slope -----------");
+
+const coordinates = [
+  [19, 3, 20, 3], // horizontal line, slope should be 0
+  [10, 50, 30, 150], // positive slope, should be 5
+  [2, 7, 4, -7], // negative slope, should be -7
+  [15, 45, 12, 60], // negative slope, should be -5
+  [10, 20, 20, 80], // positive slope, should be 6
+  [5, 5, 5, 10], // vertical line, slope should be "undefined"
+  [-10, 6, -10, 3], // vertical line, slope should be "undefined"
+];
+
+for(const item of coordinates){
+    const result = slope(item)
+    console.log(`points: ${item} \nresult: "${result}"\n`);
+}
+
