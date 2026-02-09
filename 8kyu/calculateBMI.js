8cd3c621293f3e8dacad0f30b789ea7ee8390201
Karla G. <k.grajalesluna@gmail.com>
2026-02-09 22:09:00 +0000

    test: add test cases to check if function works as expected

--- 8kyu/calculateBMI.js
+++ 8kyu/calculateBMI.js
@@ -6,14 +6,31 @@
 // if bmi > 30 return "Obese"
 
 function bmi(weight, height) {
- let bmi = weight / (height * height); // or we can square with ** 2.
-    if(bmi <= 18.5) {
-      return "Underweight";
-    }else if(bmi <= 25.0) {
-      return "Normal";  
-    }else if(bmi <= 30.0) {
-      return "Overweight"; 
-    }   
-    return "Obese";
+  let bmi = weight / (height * height); // or we can square with ** 2.
+  if (bmi <= 18.5) {
+    return "Underweight";
+  } else if (bmi <= 25.0) {
+    return "Normal";
+  } else if (bmi <= 30.0) {
+    return "Overweight";
+  }
+  return "Obese";
+}
+
+
+
+console.log("----------- bmi -----------");
+
+let weightHeight = [
+  [50, 1.8], // "Underweight"
+  [80, 1.8], // "Normal"
+  [90, 1.75], // "Overweight"
+  [110, 1.8], // "Obese"
+];
+
+
+for (const [weight, height] of weightHeight) {
+  const result = bmi(weight, height);
+  console.log(`weight: ${weight},  height: ${height}   \nresult: "${result}"\n`);
 }
 
