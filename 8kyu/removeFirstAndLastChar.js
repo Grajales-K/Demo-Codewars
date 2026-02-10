1466c80c3fd57ac7c693ccfeb9e4dea2787fe84c
Karla G. <k.grajalesluna@gmail.com>
2026-02-10 23:09:32 +0000

    feat: add string truncation logic
    - Implement removeChar using a manual for-loop for logic practice.
    - Implement removeChar using .slice() for production-ready code.

---
+++ 8kyu/removeFirstAndLastChar.js
@@ -0,0 +1,28 @@
+// Remove First and Last Character
+// Task
+// Your goal is to write a function that removes the first and last characters of a string. You're given one parameter, the original string.
+
+// Important: Your function should handle strings of any length ≥ 2 characters. For strings with exactly 2 characters, return an empty string.
+
+// Examples
+// 'eloquent' --> 'loquen'
+// 'country'  --> 'ountr' 
+// 'person'   --> 'erso'
+// 'ab'       --> '' (empty string)
+// 'xyz'      --> 'y'
+
+
+// function removeChar(str) {
+//   return str.slice(1, -1);
+// }
+
+function removeChar(str){
+    let newString = "";
+
+    for(i = 1; i < str.length -1; i++){
+        newString += str[i];
+    }
+
+    return newString;
+}
+
