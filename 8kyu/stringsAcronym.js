4a12dbcdbab7001fcb934435a4c3a6a25d9072ef
Karla G. <k.grajalesluna@gmail.com>
2026-02-11 22:48:59 +0000

    feat: add a function that loop through the string and returns initials by finding whitespace and capitalizing the character and index + 1.

---
+++ 8kyu/stringsAcronym.js
@@ -0,0 +1,22 @@
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+// The output should be two capital letters with a dot separating them.
+// It should look like this:
+// Sam Harris => S.H
+// patrick feeney => P.F
+
+// I need to extract the initials from a string. First, I initialze my
+// result with the first character in uppercase. Then, I iterate through the 
+// string; whenever I encounter a space, I target the character at the next index
+// capitalize it, and concatenate it to my result using a dot as separator.
+
+function abbrevName(name){
+    let acronym = name[0].toUpperCase();
+
+    for(let i = 0; i < name.length; i++){
+        if(name[i] === " "){
+            acronym = acronym + "." + name[i + 1].upperCase();
+        }
+    }
+    return acronym
+}
+
