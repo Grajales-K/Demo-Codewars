6ff6dbf0e5f8caad96da9a5ab8750c00dbc67e6e
Karla G. <k.grajalesluna@gmail.com>
2026-02-18 23:42:43 +0000

    test: add tests for ordinal number conversion

---  get-ordinal-number.test.js
+++  get-ordinal-number.test.js
@@ -1,1 +1,34 @@
-test("converts 1 to an ordinal number", function () {});
+test("works for any number ending in 1", function () {
+  expect(getOrdinalNumber(1)).toEqual("1st");
+  expect(getOrdinalNumber(2)).toEqual("2nd");
+  expect(getOrdinalNumber(3)).toEqual("3rd");
+  expect(getOrdinalNumber(11)).toEqual("11th");
+  expect(getOrdinalNumber(21)).toEqual("21st");
+  expect(getOrdinalNumber(31)).toEqual("31st");
+});
+
+function test(name, testFunction) {
+  // Call the passed test function
+  testFunction();
+}
+
+function getOrdinalNumber(num) {
+  if(num === 11){
+    return num + "th";
+  }
+  if(num === 21){
+    return num + "st";
+  }
+  if(num === 1){
+    return num + "st";
+  }
+  if(num === 2){
+    return num + "nd";
+  }
+  if(num === 3){
+    return num + "rd";
+  }
+}
+
+
+h
