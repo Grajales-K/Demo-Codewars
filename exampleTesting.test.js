e645faa515698228af94ec70db792275576ac2b6
Karla G. <k.grajalesluna@gmail.com>
2026-02-18 23:40:35 +0000

    test: tdd test for even and odd numbers

---
+++ exampleTesting.test.js
@@ -0,0 +1,30 @@
+
+// create a function that take three numbers as 
+// parameters and returns the largest of the three numbers.
+
+// function largestNum(num1, num2, num3) {}
+
+// console.assert(largestNum(5, 3, 7) === 7, 'largest number in third position');
+// console.assert(largestNum(5, 9, 7) === 7, "largest number in third position");
+// console.assert(largestNum(9, 3, 7) === 7, "largest number in third position");
+// console.assert(largestNum(1.3, 2.7, 3.1) === 7, "largest number in third position");
+// console.assert(largestNum(-3, -4, -5) === 7, "largest number in third position");
+// function isEven(n) {
+//   return n % 2 === 0;
+// }
+
+function isEven(n) {
+ return n % 2 === 0
+
+}
+
+
+test("isEven should return true for even numbers", () => {
+  let result = isEven(2);
+  expect(result).toBe(true); 
+})
+
+test("isEven should return false for odd numbers", () => {
+  let result = isEven(3); 
+  expect(result).toBe(false); 
+});
