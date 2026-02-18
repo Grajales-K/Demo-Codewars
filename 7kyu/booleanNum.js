006551e0e2e687f80d6ef75a0e9cdccf533387c9
Karla G. <k.grajalesluna@gmail.com>
2026-02-18 23:41:43 +0000

    test: add console.assert cases

---
+++ 7kyu/booleanNum.js
@@ -0,0 +1,13 @@
+// create a function that take three numbers as
+// parameters and returns the largest of the three numbers.
+
+// function largestNum(num1, num2, num3) {}
+
+// console.assert(largestNum(5, 3, 7) === 7, 'largest number in third position');
+// console.assert(largestNum(5, 9, 7) === 7, "largest number in third position");
+// console.assert(largestNum(9, 3, 7) === 7, "largest number in third position");
+// console.assert(largestNum(1.3, 2.7, 3.1) === 7, "largest number in third position");
+// console.assert(largestNum(-3, -4, -5) === 7, "largest number in third position");
+// function isEven(n) {
+//   return n % 2 === 0;
+// }
