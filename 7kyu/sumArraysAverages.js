3abed849d6b9fd51ddf3215196fc6d84a2322429
Karla G. <k.grajalesluna@gmail.com>
2026-05-04 22:11:19 +0100

    feat: add function to calculate sum of averages from an array of arrays

---
+++ 7kyu/sumArraysAverages.js
@@ -0,0 +1,25 @@
+// 7 kyu Sum of Array Averages
+
+// Program a function that takes in an array of arrays of 
+// numbers and returns the sum of the averages of the arrays.
+
+// Example
+// [
+//   [3, 4, 1, 3, 5, 1, 4], #  (3 + 4 + 1 + 3 + 5 + 1 + 4) / 7 =  3
+//   [21, 54, 33, 21, 77]   # (21 + 54 + 33 + 21 + 77)     / 5 = 41.2
+// ]
+
+// result: 3 + 41.2 = 44.2
+
+
+
+function sumAverage(arrays) {
+  const average = arrays.map((subArr) => {
+    const sum = subArr.reduce((curr, accu) => curr + accu, 0);
+    return sum / subArr.length;
+  });
+
+  const totalSum = average.reduce((curr, accu) => curr + accu, 0);
+
+  return totalSum;
+}
