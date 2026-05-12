cd55b203cb59d5b35d32b436b243fa3e3e943194
Karla G. <k.grajalesluna@gmail.com>
2026-05-12 23:40:37 +0100

    feat: add function to swap values in an array

---
+++ 8kyu/reverseArray.js
@@ -0,0 +1,21 @@
+
+// 8 kyu  Swap Values
+
+
+// I would like to be able to pass an array with two elements 
+// to my swapValues function to swap the values. However it 
+// appears that the values aren't changing.
+
+// Can you figure out what's wrong here?
+
+
+function swapValues() {
+  return arguments[0].reverse();
+}
+
+
+function swapValues(args) {
+  let temp = args[0];
+  args[0] = args[1];
+  args[1] = temp;
+}
