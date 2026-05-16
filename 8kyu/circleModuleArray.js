58e51b8a43cb6ba6e5a46c942ada02f84ed7806d
Karla G. <k.grajalesluna@gmail.com>
2026-05-16 23:28:41 +0100

    feat: add logic to duckDuckGoose function for player selection in circle

--- 8kyu/circleModuleArray.js
+++ 8kyu/circleModuleArray.js
@@ -19,10 +19,19 @@
 // 5000 ≤ Position ≤ 50000
 
 function duckDuckGoose(players, goose) {
+  let number = goose - 1;
+
+  let numberInCircle = number % players.length;
+
+  let playerChosen = players[numberInCircle];
+
+  return playerChosen.name;
+}
+
+function duckDuckGoose(players, goose) {
   let player = players;
   let num = goose;
 
   return player[(num - 1) % player.length].name;
 }
-
 
