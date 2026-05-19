c3de3ccc505da7026f32d1e6ba9650ee10e711df
Karla G. <k.grajalesluna@gmail.com>
2026-05-19 23:31:50 +0100

    feat: implement largestNum function to return the largest of three numbers with test cases

--- 7kyu/booleanNum.js
+++ 7kyu/booleanNum.js
@@ -1,13 +1,27 @@
 // create a function that take three numbers as
 // parameters and returns the largest of the three numbers.
 
-function largestNum(num1, num2, num3) {}
+function largestNum(num1, num2, num3) {
+  return Math.max(...arguments);
+}
 
-console.assert(largestNum(5, 3, 7) === 7, 'largest number in third position');
-console.assert(largestNum(5, 9, 7) === 7, "largest number in third position");
-console.assert(largestNum(9, 3, 7) === 7, "largest number in third position");
-console.assert(largestNum(1.3, 2.7, 3.1) === 7, "largest number in third position");
-console.assert(largestNum(-3, -4, -5) === 7, "largest number in third position");
+
+function largestNum(num1, num2, num3) {
+  return Math.max(num1, num2, num3);
+}
+
+console.assert(largestNum(5, 3, 7) === 7, 'Largest number in third position');
+console.assert(largestNum(5, 9, 7) === 9, 'Largest number in second position');
+console.assert(largestNum(9, 3, 7) === 9, 'Largest number in first position');
+console.assert(
+  largestNum(1.3, 2.7, 3.1) === 3.1,
+  'Failed with decimal numbers'
+);
+console.assert(largestNum(-3, -4, -5) === -3, 'Failed with negative numbers');
+
+
+
+
 function isEven(n) {
   return n % 2 === 0;
 }
